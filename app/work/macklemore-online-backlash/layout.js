c31931b0f0fb7b7ba8ca7c02cc87d43b46ebd3bf
Reggie <reggiemagtibay@gmail.com>
2026-09-25 14:34:45 +0800

    Add route metadata for Macklemore, Palestine and the Limits of Online Backlash

---
+++ app/work/macklemore-online-backlash/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  "title": "Macklemore, Palestine and the Limits of Online Backlash",
+  "description": "An independent analysis of 1,020 online comments examining attitudes toward Macklemore, Palestine advocacy and removal from Ed Sheeran’s U.S. tour."
+};
+
+export default function WorkLayout({ children }) {
+  return children;
+}
