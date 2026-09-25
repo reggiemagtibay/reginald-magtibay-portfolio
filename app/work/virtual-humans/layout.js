c4b5166e11e477383b08b00ba4575706999e29a9
Reggie <reggiemagtibay@gmail.com>
2026-09-25 14:34:32 +0800

    Add route metadata for Anthropomorphism and diversity in virtual humans

---
+++ app/work/virtual-humans/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  "title": "Anthropomorphism and diversity in virtual humans",
+  "description": "Published research examining how anthropomorphism, social presence and information quality shape Gen Z consumers’ trust, attitudes and purchase intentions."
+};
+
+export default function WorkLayout({ children }) {
+  return children;
+}
